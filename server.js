691d898767e7282fe15208675ee63c8159a77338
rmyers <rmyers@euro-pro.com>
2014-04-30 14:50:44 -0400

    re-factor to move out cb for future testing.

--- server.js
+++ server.js
@@ -34,30 +34,8 @@ var server = app.listen(port, function(){
 
 })
 
-server.on('ready', function(){
-    console.log('server ready')
-    // then let's move onto johnny 5, and the arduino
-
-    var five = require('johnny-five')
-    board = new five.Board()
-    board.on('ready', function(){
-        // arduino magic
-
-        forEach(tanks, function(tank, index){
-            // setup outlets
-            if(tank.outlets){
-                for(var i = 0 ; i < tank.outlets.length ; i++){
-                    var tempPin = new five.pin({addr : tank.outlets[i].pin})
-                    // this is so we can write to it in the routing object.
-                    tanks[index].outlets[i].pin = tempPin
-
-                }
-            }
-
-        })
-    })
-
-})
+var serverReadyCB = require('./lib/server-ready-cb')(tanks)
+server.on('ready', serverReadyCB)
 
 
 exports = module.exports = app
