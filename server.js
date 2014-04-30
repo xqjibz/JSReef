7282f9aceebdedec0e0532cbb8d53bf48bb8082d
rmyers <rmyers@euro-pro.com>
2014-04-30 13:47:12 -0400

    up to arduino stage here, with mongo and events.

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ var     express         = require('express')
     ,   mongo           = require('mongoskin')
     ,   argv            = require('./options')
     ,   EventEmitter    = require('events').EventEmitter
+    ,   util            = require('util')
 
 var port = process.env.PORT || argv.port || 3000; // set our port
-
+util.inherits(app, EventEmitter)
 app.configure(function() {
     app.use(express.static(__dirname + '/public')) 	// set the static files location /public/img will be /img for users
     app.use(express.logger('dev')) 					// log every request to the console
@@ -17,16 +18,33 @@ app.configure(function() {
 // routes
 require('./app/routes')(app)
 
+
+// tanks/controllers array
+var tanks = []
+var db = mongo.db("mongodb://localhost/reef", {safe : true})
 // start app
 var server = app.listen(port, function(){
     //console.log('listening on ' + server.address().port)
     // six one 1/2 dozen the other, let's connect to mongo here.
 
-    // find the tanks we're controlling, and we'll put them into an array
-
-
-    // then let's move onto johnny 5, and the arduino
+    db.collection('tanks').findItems({}, function(err, results){
+        tanks.push.apply(tanks, results)
+        server.emit('ready')
+    })
 
 })
+
+server.on('ready', function(){
+    console.log('server ready')
+    // then let's move onto johnny 5, and the arduino
+
+    var five = require('johnny-five')
+    board = new five.Board()
+    board.on('ready', function(){
+        // arduino magic
+    })
+
+})
+
 
 exports = module.exports = app
