c1d9f780e4315bfd69ad6a87314e4859366add86
rmyers <rmyers@euro-pro.com>
2014-05-03 21:07:43 -0400

    fix jade issue, and move pin to led for functions access

--- public/js/controllers/outletscontroller.js
+++ public/js/controllers/outletscontroller.js
@@ -2,11 +2,24 @@
 angular.module('OutletsCtrl', []).controller('OutletsController', function ( $route, $scope, $location, Restangular, SharedData)  {
 
 
-    var selectedTank = SharedData.getSelectedTank()
+
     //Restangular.setBaseUrl('http://localhost:3000')
-    $scope.outlets = Restangular.all('tanks/' + selectedTank + '/outlets').getList().$object
 
 
+    $scope.updateOutlets = function(){
+        $scope.selectedTank = SharedData.getSelectedTank()
+        if($scope.selectedTank !== null){
+            $scope.outlets = Restangular.all('tanks/' + $scope.selectedTank + '/outlets').getList().$object
+        }
+
+    }
+
+    $scope.$watch(function (){
+        // just in case we need to add something else to the watch.
+        return {
+            tankSelector : SharedData.getSelectedTank()
+        }
+    }, $scope.updateOutlets, true)
 })
 
 
