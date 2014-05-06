2c51a5e7896a776fc25b926c9d5bd2e43804970f
rmyers <rmyers@euro-pro.com>
2014-05-05 23:45:27 -0400

    updated to use socket.io with expressJS

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 var     express         = require('express')
     ,   app             = express()
+
+    ,   server          = require('http').createServer(app)
     ,   mongo           = require('mongoskin')
     ,   argv            = require('./options')
     ,   EventEmitter    = require('events').EventEmitter
@@ -12,7 +14,11 @@ var tanks = []
 //TODO This connects here, and really should do something other than move on.
 var db = mongo.db("mongodb://localhost/reef", {safe : true})
 
-util.inherits(app, EventEmitter)
+var io = require('socket.io').listen(server)
+
+
+
+
 
 app.configure(function() {
     app.use(express.static(__dirname + '/public')) 	// set the static files location /public/img will be /img for users
@@ -25,23 +31,30 @@ app.configure(function() {
 
 require('./app/routes')(app, tanks)
 
-
-
-// start app
-var server = app.listen(port, function(){
-    //console.log('listening on ' + server.address().port)
-    // six one 1/2 dozen the other, let's connect to mongo here.
-
+server.listen(port, function(){
+    console.log('server ready')
     db.collection('tanks').findItems({}, function(err, results){
-        tanks.push.apply(tanks, results)
-        server.emit('ready')
-        //console.log(util.inspect(tanks, true,null))
+    tanks.push.apply(tanks, results)
+    //server.emit('ready')
+    //console.log(util.inspect(tanks, true,null))
     })
 
 })
 
-var serverReadyCB = require('./lib/server-ready-cb')(tanks, argv)
-server.on('ready', serverReadyCB)
+// start app
+//server.createServer(app)
 
+//var server = app.listen(port, function(){
+//    //console.log('listening on ' + server.address().port)
+//    // six one 1/2 dozen the other, let's connect to mongo here.
+//
+
+//})
+//
+//var serverReadyCB = require('./lib/server-ready-cb')(tanks, argv)
+//server.on('ready', serverReadyCB)
+//server.on('ready', function(){
+//    console.log('server ready')
+//})
 
 exports = module.exports = app
